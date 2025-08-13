a93ce79b9989ced2d5072314467b282870751fef
Thanattsitt S. <166811468+Pigletpeakkung@users.noreply.github.com>
2025-08-13 18:29:19 +0700

    Create resume-data.js

---
+++ resume-data.js
@@ -0,0 +1,9 @@
+const resumeData = {
+  // Paste the entire JSON data object here from the previous response
+  // (The data.json content goes here)
+};
+
+// For Node.js
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = resumeData;
+}
